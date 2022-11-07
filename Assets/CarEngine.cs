9e12d87877846b25f6284a84cfc6762db4a5e73e
moon920110 <moon920110@gmail.com>
2022-11-07 18:12:07 +0900

    Enable wheel control

--- Assets/CarEngine.cs
+++ Assets/CarEngine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,44 +13,40 @@ public class CarEngine : MonoBehaviour
 
     public float maxMotorTorque = 80f;
     public float currentSpeed;
-    public float maxSpeed = 100f;
+    public float maxSpeed = 1000f;
 
     public Vector3 centerOfMass;
 
     private List<Transform> nodes;
-    private int currentNode = 0;
+
+    private float gasInput;
+    private float brakeInput;
+    private float steerInput;
 
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody>().centerOfMass = centerOfMass;
-
-        Transform[] pathTransform = path.GetComponentsInChildren<Transform>();
-        nodes = new List<Transform>();
-        
-
-        for (int i = 0; i < pathTransform.Length; i++)
-        {
-            if (pathTransform[i] != path.transform)
-            {
-                nodes.Add(pathTransform[i]);
-            }
-        }
+        print("LogiSteeringInitialize: " + LogitechGSDK.LogiSteeringInitialize(false));
     }
 
     // Update is called once per frame
-    private void FixedUpdate()
+    private void Update()
     {
+        if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConnected((int)LogitechKeyCodes.FirstIndex))
+        {
+            steerInput = GetAxis(AxisName.SteeringHorizontal);
+            gasInput = GetAxis(AxisName.GasVertical);
+            brakeInput = GetAxis(AxisName.BrakeVertical);
+        }
         ApplySteer();
         Drive();
-        CheckWaypointDistance();
     }
 
     private void ApplySteer()
     {
-        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
-        float newsteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        float newsteer = steerInput * maxSteerAngle;
         //
         //print(relativeVector);
         WheelFL.steerAngle = newsteer;
@@ -59,31 +56,42 @@ public class CarEngine : MonoBehaviour
     private void Drive()
     {
         currentSpeed = 2 * Mathf.PI * WheelFL.radius * WheelFL.rpm * 60 / 1000;
-
-        if (currentSpeed < maxSpeed)
+        if (currentSpeed < 0)
         {
-            WheelFL.motorTorque = maxMotorTorque;
-            WheelFR.motorTorque = maxMotorTorque;
+            currentSpeed = 0f;
         }
-        else
+
+        // TODO: brake implementation
+        float throttle = (gasInput - brakeInput * 3f) * maxSpeed ;
+
+        if (currentSpeed == 0f)
         {
-            WheelFL.motorTorque = 0;
-            WheelFR.motorTorque = 0;
-
+            throttle = 0f;
         }
-    }
+        print($"{currentSpeed}, {throttle}");
+        WheelFL.motorTorque = currentSpeed >= 0f ? throttle : 0;
+        WheelFR.motorTorque = currentSpeed >= 0f ? throttle : 0;
+   }
 
-    private void CheckWaypointDistance()
+        private static float GetAxis(AxisName axisName)
     {
-        if (Vector3.Distance(transform.position, nodes[currentNode].position) < 0.5f) {
-            if (currentNode == nodes.Count - 1)
-            {
-                currentNode = 0;
-            }
-            else
-            {
-                currentNode++;
-            }
+        LogitechGSDK.DIJOYSTATE2ENGINES rec;
+        rec = LogitechGSDK.LogiGetStateUnity(0);
+
+        switch(axisName)
+        {
+            case AxisName.SteeringHorizontal: return rec.lX / (float)Int16.MaxValue;
+            case AxisName.GasVertical: return (rec.lY - Int16.MaxValue) / (float)Int16.MinValue;
+            case AxisName.BrakeVertical: return (rec.rglSlider[0] - Int16.MaxValue) / (float)Int16.MinValue;
         }
+        return 0f;
     }
 }
+
+enum AxisName
+    {
+        SteeringHorizontal,
+        GasVertical,
+        ClutchVertical,
+        BrakeVertical,
+    }
